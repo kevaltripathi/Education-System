f7d8ad0c9563fdb7afb41168a26c239994e93b87
David Teresi <dkteresi@gmail.com>
2021-09-18 14:16:45 -0400

    Add time multiplier

--- App.js
+++ App.js
@@ -12,20 +12,22 @@ const G = 6.67e-11;
 let centerMass = 5.9e24;
 const metres_per_px = 384400000 / 60; // pixels
 
+const time_multiplier = 10;
+
 const App = () => {
     const [objects, setObjects] = useState(
         [
             {
                 x: centerX - 60,
                 y: centerY,
-                velocity: [0, -0.4],
                 mass: 7.3e22,
+                velocity: [0, -0.4 * time_multiplier],
             },
             {
                 x: centerX,
                 y: centerY,
-                velocity: [0, 0.0],
                 mass: 5.9e24,
+                velocity: [0, 0.0 * time_multiplier],
             }
         ]
     );
@@ -40,7 +42,7 @@ const App = () => {
                     let distance = Math.sqrt((object2.x-object.x) ** 2 + (object2.y-object.y) ** 2);
                     let direction = Math.atan2(object.x - object2.x, object.y - object2.y);
                     let force = -G * object.mass * object2.mass / (distance * metres_per_px) ** 2;
-                    let accel = force / object.mass;
+                    let accel = force / object.mass * time_multiplier ** 2;
                     velocity = [
                         velocity[0] + accel * Math.sin(direction),
                         velocity[1] + accel * Math.cos(direction),
