b10706e737e3c24462da0ec46d01b1550f7fedcb
David Teresi <dkteresi@gmail.com>
2021-09-18 13:02:07 -0400

    orbits

--- App.js
+++ App.js
@@ -8,15 +8,42 @@ const RADIUS = 25;
 const centerX = WIDTH / 2 - RADIUS;
 const centerY = HEIGHT / 2 - RADIUS;
 
+const G = 6.67e-11;
+let centerMass = 5.9e24;
+const metres_per_px = 384400000 / 60; // pixels
+
 const App = () => {
-    const [position, setPosition] = useState({x: centerX, y: centerY});
+    const [position, setPosition] = useState({
+        x: centerX - 60,
+        y: centerY,
+        velocity: [0, -0.4],
+        mass: 7.3e22,
+    });
+    const distance = Math.sqrt((centerX-position.x) ** 2 + (centerY-position.y) ** 2);
+    const direction = Math.atan2(position.x - centerX, position.y - centerY);
 
     const updateHandler = ({ touches, screen, layout, time }) => {
         let move = touches.find(x => x.type === "move");
         if (move) {
             setPosition({
                 x: position.x + move.delta.pageX,
-                y: position.y + move.delta.pageY
+                y: position.y + move.delta.pageY,
+                velocity: position.velocity,
+                mass: position.mass,
+            });
+        }
+        else {
+            let force = -G * position.mass * centerMass / (distance * metres_per_px) ** 2;
+            let accel = force / position.mass;
+            let velocity = [
+                position.velocity[0] + accel * Math.sin(direction),
+                position.velocity[1] + accel * Math.cos(direction),
+            ];
+            setPosition({
+                x: position.x + velocity[0],
+                y: position.y + velocity[1],
+                velocity: velocity,
+                mass: position.mass
             });
         }
     };
@@ -24,7 +51,10 @@ const App = () => {
     return (
         <GameLoop style={styles.container} onUpdate={updateHandler}>
             <SafeAreaView>
-                <Text>The Distance between the planets is: {Math.round(Math.sqrt((centerX-position.x) ** 2 + (centerY-position.y) ** 2))}</Text>
+                <Text></Text>
+                <Text></Text>
+                <Text>The Distance between the planets is: {Math.round(distance)}</Text>
+                <Text>velocity: {position.velocity}</Text>
             </SafeAreaView>
 
             <View style={[styles.player, { left: centerX, top: centerY, backgroundColor: "green" }]} />
