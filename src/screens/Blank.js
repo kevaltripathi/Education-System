cc138d3a85bbec719dab7a095b2dbb80eebe260b
Anna Lai <annateresa.lai@mail.utoronto.ca>
2021-09-18 18:32:33 -0400

    show a giant big earth on blank page

--- src/screens/Blank.js
+++ src/screens/Blank.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import {Text, View} from "react-native";
+import {Text, View, Image} from "react-native";
 
 export const Blank = () => {
 
     return (
         <View>
-            <Text>Hello World!</Text>
+            <Text>Welcome to Gravity Simulation!</Text>
+            <Text>Let's learn some physics together. </Text>
+            <Image source={require('./../../assets/img/planet-13.png')} />
         </View>
     );
 };
