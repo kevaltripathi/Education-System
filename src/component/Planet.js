4ffe18c52fad5a60b165dfe9e97c1b699b751a83
Anna Lai <annateresa.lai@mail.utoronto.ca>
2021-09-19 01:15:10 -0400

    Show random planet

--- src/component/Planet.js
+++ src/component/Planet.js
@@ -2,11 +2,13 @@ import React from "react";
 import {Image, StyleSheet, View} from "react-native";
 
 const RADIUS = 20;
+const rndInt = Math.floor(Math.random() * 4)
+const planets = {0: require("./../../assets/img/planet-01.png"), 1: require("./../../assets/img/planet-08.png"), 2: require("./../../assets/img/planet-12.png"), 3: require("./../../assets/img/planet-13.png")}
 
 const Planet = (props) => {
     const {position} = props;
     return (
-        <Image source={require('./../../assets/img/planet-08.png')} style={[styles.planet, {left: (position.x - RADIUS), top: (position.y - RADIUS)}]}/>
+        <Image source={planets[rndInt]} style={[styles.planet, {left: (position.x - RADIUS), top: (position.y - RADIUS)}]}/>
     );
 };
 
