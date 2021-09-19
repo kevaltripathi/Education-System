aaac3cd25eeaa890e434fd029c708062ef292414
David Teresi <dkteresi@gmail.com>
2021-09-19 00:38:32 -0400

    re-add toolbar

--- src/screens/Simulation.js
+++ src/screens/Simulation.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {GameEngine} from "react-native-game-engine";
+import {Button, StyleSheet, Dimensions, View, Text, SafeAreaView} from "react-native";
+import {Icon} from "react-native-elements";
 import {Planet} from "../component/Planet";
 import {createPlanet} from "../systems/createPlanet";
 
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 const metres_per_px = 384400000 / 60;
 const initial_time_multiplier = 10;
 const G = 6.67e-11;
@@ -39,11 +42,52 @@ const Move = (entities) => {
 
 export const Simulation = () => {
     return (
-        <GameEngine
-            systems = {[Move, Gravity, createPlanet]}
-            entities = {[
-                {position: {x: 100, y: 100}, velocity: {x:0, y:-1}, mass: 4, renderer: <Planet/>},
-                {position: {x: 300, y: 300}, velocity: {x:0, y:1}, mass: 20, renderer: <Planet/>}
-            ]}/>
+        [
+            <GameEngine
+                systems = {[Move, Gravity, createPlanet]}
+                entities = {[
+                    {position: {x: 100, y: 400}, velocity: {x:0, y:-0.5}, mass: 4, renderer: <Planet/>},
+                    {position: {x: 150, y: 300}, velocity: {x:0, y:0.5}, mass: 20, renderer: <Planet/>}
+                ]}/>,
+            <SafeAreaView style={styles.bottom}>
+                <Icon style={styles.bottomIcon} name={"pause"} onPress={() => {}} />
+                <View style={styles.vline} />
+                <Icon style={styles.bottomIcon} name="fast-rewind" onPress={() => {}} />
+                <Text style={styles.bottomText}>1x</Text>
+                <Icon style={styles.bottomIcon} name="fast-forward" onPress={() => {}} />
+            </SafeAreaView>
+        ]
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000"
+    },
+    bottom: {
+        backgroundColor: "#FFF",
+        position: "absolute",
+        width: WIDTH,
+        display: "flex",
+        flexDirection: "row",
+        bottom: 0,
+        padding: 10,
+    },
+    bottomIcon: {
+        height: 30,
+        width: 30,
+    },
+    bottomText: {
+        marginTop: 5,
+        marginLeft: 15,
+        marginRight: 15,
+        includeFontPadding: false,
+    },
+    vline: {
+        width: 1,
+        backgroundColor: "#CCC",
+        marginLeft: 15,
+        marginRight: 15,
+    }
+});
